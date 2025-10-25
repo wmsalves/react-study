c7d69bb82e9612db4e8e238a94ef2e02c5c983ee
Infinity <wemersoncontas22@gmail.com>
2025-10-25 10:47:03 -0300

    feat: initial project setup for accessibility study (Next.js)

---
+++ curso-ally/src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"
+      ></link>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
