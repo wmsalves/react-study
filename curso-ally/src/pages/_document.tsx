4dc98b844d703d02744d68193297d2731fac6528
Infinity <wemersoncontas22@gmail.com>
2025-10-25 16:59:40 -0300

    fix(a11y): correct color contrast for text and links

--- curso-ally/src/pages/_document.tsx
+++ curso-ally/src/pages/_document.tsx
@@ -1,19 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet"
-      ></link>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
