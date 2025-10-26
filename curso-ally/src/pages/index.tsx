82146ef2563fd18032cb95ddb38e3f8c6a58aba9
Infinity <wemersoncontas22@gmail.com>
2025-10-26 11:59:51 -0300

    docs(a11y): final takeaways on accessibility principles over tools

--- curso-ally/src/pages/index.tsx
+++ curso-ally/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import Image from "next/image";
 
+import Image from "next/image";
 import Head from "next/head";
 
 import LogoImg from "../assets/logo.svg";
