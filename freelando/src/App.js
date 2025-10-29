e5d5d2b0071f72ec2f5dd81f5c8e8b23404d3c0c
Infinity <wemersoncontas22@gmail.com>
2025-10-29 13:17:00 -0300

    feat: create ListaSuspensa component with state, dynamic options, and initial keyboard navigation

--- freelando/src/App.js
+++ freelando/src/App.js
@@ -3,8 +3,39 @@ import { Botao } from "./componentes/Botao/Botao";
 import { CampoTexto } from "./componentes/CampoTexto/CampoTexto";
 import { Card } from "./componentes/Card/Card";
 import { Estilos } from "./componentes/EstilosGlobais/Estilos";
+import { ListaSupensa } from "./componentes/ListaSuspensa/ListaSuspensa";
 import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema";
 import { Tipografia } from "./componentes/Tipografia/Tipografia";
+
+const estadosBrasileiros = [
+  { text: "Acre", value: "AC" },
+  { text: "Alagoas", value: "AL" },
+  { text: "Amapá", value: "AP" },
+  { text: "Amazonas", value: "AM" },
+  { text: "Bahia", value: "BA" },
+  { text: "Ceará", value: "CE" },
+  { text: "Distrito Federal", value: "DF" },
+  { text: "Espírito Santo", value: "ES" },
+  { text: "Goiás", value: "GO" },
+  { text: "Maranhão", value: "MA" },
+  { text: "Mato Grosso", value: "MT" },
+  { text: "Mato Grosso do Sul", value: "MS" },
+  { text: "Minas Gerais", value: "MG" },
+  { text: "Pará", value: "PA" },
+  { text: "Paraíba", value: "PB" },
+  { text: "Paraná", value: "PR" },
+  { text: "Pernambuco", value: "PE" },
+  { text: "Piauí", value: "PI" },
+  { text: "Rio de Janeiro", value: "RJ" },
+  { text: "Rio Grande do Norte", value: "RN" },
+  { text: "Rio Grande do Sul", value: "RS" },
+  { text: "Rondônia", value: "RO" },
+  { text: "Roraima", value: "RR" },
+  { text: "Santa Catarina", value: "SC" },
+  { text: "São Paulo", value: "SP" },
+  { text: "Sergipe", value: "SE" },
+  { text: "Tocantins", value: "TO" },
+];
 
 function App() {
   return (
@@ -29,7 +60,7 @@ function App() {
               </Row>
               <Row>
                 <Col lg={4} md={4} sm={4}>
-                  <CampoTexto titulo="Estado" />
+                  <ListaSupensa titulo="Estado" opcoes={estadosBrasileiros} />
                 </Col>
                 <Col lg={8} md={8} sm={8}>
                   <CampoTexto titulo="Cidade" />
