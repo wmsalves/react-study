a6b72b73dea9358abae0dc9752e49c6d94c7f8f7
Infinity <wemersoncontas22@gmail.com>
2025-10-31 09:26:59 -0300

    feat: integrate authentication api, implement login, and store tokens

--- freelando/src/App.jsx
+++ freelando/src/App.jsx
@@ -2,14 +2,15 @@ import { RouterProvider } from "react-router-dom";
 import { Estilos } from "./componentes/EstilosGlobais/Estilos";
 import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema";
 import { router } from "./router/router";
+import { SessaoUsuarioProvider } from "./contexto/SessaoUsuario";
 
 function App() {
   return (
     <ProvedorTema>
       <Estilos />
-      <RouterProvider router={router} />
+      <SessaoUsuarioProvider>
+        <RouterProvider router={router} />
+      </SessaoUsuarioProvider>
     </ProvedorTema>
   );
 }
-
-export default App;
--- freelando/src/contexto/SessaoUsuario.jsx
+++ freelando/src/contexto/SessaoUsuario.jsx
@@ -1,12 +1,38 @@
 import { createContext, useContext } from "react";
+import http from "../http";
+import { ArmazenadorToken } from "../utils/ArmazenadorToken";
 
 const SessaoUsuarioContext = createContext({
-  usuarioEstaLogado: false,
-  login: () => null,
-  logout: () => null,
-  perfil: {},
-});
+    usuarioEstaLogado: false,
+    login: (email, senha) => null,
+    logout: () => null,
+    perfil: {}
+})
 
 export const useSessaoUsuarioContext = () => {
-  return useContext(SessaoUsuarioContext);
-};
+    return useContext(SessaoUsuarioContext)
+}
+
+export const SessaoUsuarioProvider = ({children}) => {
+
+    const login = (email, senha) => {
+        http.post('auth/login', {
+            email,
+            senha
+        })
+            .then(resposta => {
+                ArmazenadorToken.definirTokens(
+                    resposta.data.access_token,
+                    resposta.data.refresh_token,
+                )
+            })
+            .catch(erro => console.error(erro))
+    }
+
+    const value = {
+        login
+    }
+    return (<SessaoUsuarioContext.Provider value={value}>
+        {children}
+    </SessaoUsuarioContext.Provider>)
+}
