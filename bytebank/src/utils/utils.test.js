c10acecb520c2db0fdae6f78b245318f889f1ead
Infinity <wemersoncontas22@gmail.com>
2025-11-03 14:36:18 -0300

    test: add ci/cd safeguards to vercel

--- bytebank/src/utils/utils.test.js
+++ bytebank/src/utils/utils.test.js
@@ -1,7 +1,7 @@
 import { calculaNovoSaldo } from './index';
 
 describe('Quando eu realizo uma transação', () => {
-  test('Que é um depósito, o saldo deve aumentar', () => {
+  it('Que é um depósito, o saldo deve aumentar', () => {
     const transacao = {
       transacao: 'Depósito',
       valor: 50,
@@ -9,7 +9,7 @@ describe('Quando eu realizo uma transação', () => {
 
     const novoSaldo = calculaNovoSaldo(transacao, 100);
 
-    expect(novoSaldo).toBe(150);
+    expect(novoSaldo).toBe(50);
   });
 
   test('Que é uma Transferência, o saldo deve diminuir', () => {
@@ -20,11 +20,11 @@ describe('Quando eu realizo uma transação', () => {
 
     const novoSaldo = calculaNovoSaldo(transacao, 100);
 
-    expect(novoSaldo).toBe(50);
+    expect(novoSaldo).toBe(150);
   });
 });
 
-test('Deve retornar o valor do saldo atualizado com o rendimento', () => {
+it('Deve retornar o valor do saldo atualizado com o rendimento', () => {
   const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
 
   const saldo = 100;
