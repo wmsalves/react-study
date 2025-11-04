4d359b2b14577e1dbd84775c77450b4451c01e8f
Infinity <wemersoncontas22@gmail.com>
2025-11-04 16:28:17 -0300

    test(cypress): create custom command `getbydata` and use `beforeeach`

--- bytebank/cypress/e2e/start.cy.js
+++ bytebank/cypress/e2e/start.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-  })
-})
+describe('Página inicial', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+  it('Deve renderizar o h1 com o texto correto', () => {
+    cy.getByData('titulo-principal').contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    );
+  });
+});
