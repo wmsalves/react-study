c73b0af4d3bf06ed23250c44dea45c88f1f0fb0b
Infinity <wemersoncontas22@gmail.com>
2025-11-05 15:33:59 -0300

    feat(cypress): create custom login command and refactor tests

--- bytebank/cypress/e2e/formularioLogin.cy.js
+++ bytebank/cypress/e2e/formularioLogin.cy.js
@@ -1,11 +1,15 @@
 describe('Formulario de Login', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
+    cy.login('wemerson@gmail.com', '123456');
+  });
+
+  it('Deve acessar a página home', () => {
+    cy.getByData('titulo-boas-vindas').should('contain', 'Bem vindo de volta!');
   });
 
   it('Não deve permitir um email inválido', () => {
     cy.getByData('botao-login').click();
-    cy.getByData('email-input').type('wemerson@alura');
+    cy.getByData('email-input').type('neilton@alura');
     cy.getByData('senha-input').type('123456');
     cy.getByData('botao-enviar').click();
     cy.getByData('mensagem-erro')
@@ -13,7 +17,7 @@ describe('Formulario de Login', () => {
       .and('have.text', 'O email digitado é inválido');
   });
 
-  it.only('Não deve permitir um campo em branco', () => {
+  it('Não deve permitir um campo em branco', () => {
     cy.getByData('botao-login').click();
     cy.getByData('senha-input').type('123456');
     cy.getByData('botao-enviar').click();
--- bytebank/cypress/support/commands.js
+++ bytebank/cypress/support/commands.js
@@ -1,3 +1,11 @@
 Cypress.Commands.add('getByData', (seletor) => {
   return cy.get(`[data-test=${seletor}]`);
 });
+
+Cypress.Commands.add('login', (email, senha) => {
+  cy.getByData('botao-login').click();
+  cy.getByData('email-input').type(email);
+  cy.getByData('senha-input').type(senha);
+  cy.getByData('botao-enviar').click();
+  cy.url().should('contain', '/home');
+});
