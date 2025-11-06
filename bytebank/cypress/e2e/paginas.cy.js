9469a690afbf492906180b006f33af2114815431
Infinity <wemersoncontas22@gmail.com>
2025-11-06 15:04:10 -0300

    test(paginas): refactor navigation test to be responsive

--- bytebank/cypress/e2e/paginas.cy.js
+++ bytebank/cypress/e2e/paginas.cy.js
@@ -1,14 +1,20 @@
-describe('Testando múltiplas páginas', () => {
-  it('Deve conseguir acessar a página de cartões', { browser: 'edge' }, () => {
-    cy.visit('/');
-    cy.getByData('botao-login').click();
-    cy.getByData('email-input').type('wemerson@gmail.com');
-    cy.getByData('senha-input').type('123456');
-    cy.getByData('botao-enviar').click();
+import { isMobile } from '../support/utils';
 
+describe('Testando múltiplas páginas', () => {
+  it('Deve conseguir acessar a página de cartões', () => {
+    cy.login(Cypress.env('email'), Cypress.env('senha'));
+
+    cy.visit('/home');
     cy.location('pathname').should('eq', '/home');
 
-    cy.getByData('app-home').find('a').eq(1).click();
+    if (isMobile()) {
+      cy.getByData('menu-burguer').should('be.visible');
+      cy.getByData('menu-burguer').click();
+      cy.getByData('app-home').find('a').eq(2).click();
+    } else {
+      cy.getByData('app-home').find('a').eq(2).click();
+    }
+
     cy.getByData('titulo-cartoes')
       .should('exist')
       .and('have.text', 'Meus cartões');
