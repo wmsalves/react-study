d33e090fd6888248a281e71f8d76eb06027da89c
Infinity <wemersoncontas22@gmail.com>
2025-11-06 14:03:43 -0300

    test(cypress): add api login test using `cypress.env.json` for sensitive data

--- bytebank/cypress/e2e/api/api-users.cy.js
+++ bytebank/cypress/e2e/api/api-users.cy.js
@@ -52,4 +52,18 @@ describe('Realizando requisições para a API', () => {
       );
     });
   });
+  context('Realizando login via API', () => {
+    it('Deve permitir o login do usuário Wemerson Alves', () => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:8000/users/login',
+        body: Cypress.env(),
+      }).then((resposta) => {
+        expect(resposta.status).to.eq(200);
+        expect(resposta.body).is.not.empty;
+        expect(resposta.body.user).to.have.property('nome');
+        expect(resposta.body.user.nome).to.be.equal('Wemerson Alves');
+      });
+    });
+  });
 });
