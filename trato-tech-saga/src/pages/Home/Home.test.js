25987a40b1b9a79e6e7b2d61033483ca95937629
Infinity <wemersoncontas22@gmail.com>
2025-11-07 11:37:50 -0300

    test(home): configure jest mocks and async category test

--- trato-tech-saga/src/pages/Home/Home.test.js
+++ trato-tech-saga/src/pages/Home/Home.test.js
@@ -1,12 +1,13 @@
 import Home from ".";
 import { render, screen } from "../../test-utils";
 
-describe("Testando página Home", () => {
-  test("deve renderizar com categorias", () => {
-    render(<Home />);
-    const categorias = screen.getAllByTestId("home-categorias");
+jest.mock("services/categorias");
 
-    expect(categorias).toHaveLength(5);
-    expect(true).toBeTruthy();
+describe("Testando página Home", () => {
+  test("deve renderizar com categorias", async () => {
+    render(<Home />);
+    const categorias = await screen.findAllByTestId("home-categorias");
+
+    expect(categorias).toHaveLength(2);
   });
 });
