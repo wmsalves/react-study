53fb2cfdccde587aab085d418c0ee994a9cddbc9
Infinity <wemersoncontas22@gmail.com>
2025-11-07 09:21:56 -0300

    test(setup): fix jest errors with moduleNameMapper and custom render util

--- trato-tech-saga/src/pages/Home/Home.test.js
+++ trato-tech-saga/src/pages/Home/Home.test.js
@@ -1,5 +1,9 @@
+import Home from ".";
+import { render } from "../../test-utils";
+
 describe("Testando página Home", () => {
   test("testando", () => {
+    render(<Home />);
     expect(true).toBeTruthy();
   });
 });
